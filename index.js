5749fae15ec5ab55d2f0355c0bc6adf67fdd8919
joshuadazar <joshuadazar@gmail.com>
2024-04-30 17:18:20 -0500

    added cors

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+const os = require('os');
 const express = require('express');
 const { resolve } = require('path');
+const cors = require('cors');
 
 const app = express();
 const port = 3010;
 
 app.use(express.json()); // para poder parsear JSON
+
+app.use(cors({
+  origin: 'https://www.clinicanaturlich.com/'
+}));
 
 app.post('/mi-endpoint', (req, res) => {
   console.log(req.body); // imprime lo que se envió en la petición
@@ -12,5 +18,5 @@ app.post('/mi-endpoint', (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log('Servidor corriendo en http://localhost:3000');
+  console.log(`Servidor corriendo en ${os.hostname()}:3000`);
 });
