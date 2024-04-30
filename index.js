4e6653c0c03431241e9e8ed637efb08d2b3d8441
joshuadazar <joshuadazar@gmail.com>
2024-04-30 14:46:58 -0500

    set base post endpoint

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const { resolve } = require('path');
 const app = express();
 const port = 3010;
 
-app.use(express.static('static'));
+app.use(express.json()); // para poder parsear JSON
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+app.post('/mi-endpoint', (req, res) => {
+  console.log(req.body); // imprime lo que se envió en la petición
+  res.status(200).send('Recibido');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(3000, () => {
+  console.log('Servidor corriendo en http://localhost:3000');
 });
